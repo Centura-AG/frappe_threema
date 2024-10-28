803dd646b7b871c6ffec1e1dc6d2472b84c8bc65
Marc <marc.ramser@centura.ch>
2024-10-28 16:39:38 +0100

    Create DocTypes

---
+++ app_frappe_threema/threema/doctype/threema_message_log/threema_message_log.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Centura AG and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ThreemaMessageLog(Document):
+	pass
---
+++ app_frappe_threema/threema/doctype/threema_settings/test_threema_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Centura AG and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestThreemaSettings(FrappeTestCase):
+	pass
