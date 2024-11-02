8e38057adcd1aa308fe8c8ea5ba7ba11c9ad31cd
Marc <marc.ramser@centura.ch>
2024-11-02 14:22:49 +0100

    Implement Threema Center to send in basic mode to phone

--- app_frappe_threema/threema/doctype/threema_center/threema_center.py
+++ app_frappe_threema/threema/doctype/threema_center/threema_center.py
@@ -2,8 +2,36 @@
 # For license information, please see license.txt
 
 # import frappe
+import frappe
+from frappe import _, msgprint
+from app_frappe_threema.threema.doctype.threema_settings.threema_settings import send_message
 from frappe.model.document import Document
+from frappe.utils import cstr
+
 
 
 class ThreemaCenter(Document):
-	pass
+	def get_receiver_nos(self):
+		receiver_nos = []
+		if self.receiver_list:
+			for d in self.receiver_list.split("\n"):
+				receiver_no = d
+				if "-" in d:
+					receiver_no = receiver_no.split("-")[1]
+				if receiver_no.strip():
+					receiver_nos.append(cstr(receiver_no).strip())
+		else:
+			msgprint(_("Receiver List is empty. Please create Receiver List"))
+
+		return receiver_nos
+
+	@frappe.whitelist()
+	def send_message(self):
+		receiver_list = []
+		if not self.message:
+			msgprint(_("Please enter message before sending"))
+		else:
+			receiver_list = self.get_receiver_nos()
+		if receiver_list:
+			send_message(receiver_list, cstr(self.message))
+
--- app_frappe_threema/threema/doctype/threema_settings/threema_settings.js
+++ app_frappe_threema/threema/doctype/threema_settings/threema_settings.js
@@ -1,8 +1,1 @@
-// Copyright (c) 2024, Centura AG and contributors
-// For license information, please see license.txt
 
-// frappe.ui.form.on("Threema Settings", {
-// 	refresh(frm) {
-
-// 	},
-// });
