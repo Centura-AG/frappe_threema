4204487315be78127f1b061cb7b46e22d5c72591
Marc <marc.ramser@centura.ch>
2024-11-03 13:17:53 +0100

    Add threema receiver list

--- app_frappe_threema/threema/doctype/notification/notification.py
+++ app_frappe_threema/threema/doctype/notification/notification.py
@@ -3,13 +3,63 @@
 
 import frappe
 import json
+from frappe.core.doctype.role.role import get_info_based_on_role, get_user_info
 from frappe.email.doctype.notification.notification import Notification
 from app_frappe_threema.threema.doctype.threema_settings.threema_settings import send_message
 
 class CustomNotification(Notification):
+
+	def get_threema_receiver_list(self, doc, context):
+		"""return receiver list based on the doc field and role specified"""
+		receiver_list = []
+		recipients = frappe.db.get_list("Notification Recipient", filters={ "parent": self.name})
+		for recipient_name in recipients:
+			recipient = frappe.get_doc('Notification Recipient', recipient_name)
+			if recipient.condition:
+				if not frappe.safe_eval(recipient.condition, None, context):
+					continue
+			# For sending messages to the owner's mobile phone number
+			if recipient.receiver_by_document_field == "owner":
+				owner = [dict(user_name=doc.get("owner"))]
+				threema_id = get_user_info(owner, "threema_id")[0]
+				mobile_no = get_user_info(owner, "mobile_no")[0]
+				email = get_user_info(owner, "email")[0]
+
+				if bool(threema_id and threema_id.strip()):
+					receiver_list.append(threema_id)
+				elif bool(mobile_no and mobile_no.strip()):
+					receiver_list.append(mobile_no)
+				elif bool(email and email.strip()):
+					receiver_list.append(email)
+
+			# For sending messages to the contact specified in the receiver field
+			elif recipient.receiver_by_document_field:
+				receiver_list.append(doc.get(recipient.receiver_by_document_field))
+
+			# For sending messages to specified role
+			if recipient.receiver_by_role:
+				threema_id = get_info_based_on_role(
+					recipient.receiver_by_role, "threema_id", ignore_permissions=True
+				)[0]
+				mobile_no = get_info_based_on_role(
+					recipient.receiver_by_role, "mobile_no", ignore_permissions=True
+				)[0]
+				email = get_info_based_on_role(
+					recipient.receiver_by_role, "email", ignore_permissions=True
+				)[0]
+
+				if bool(threema_id and threema_id.strip()):
+					receiver_list.append(threema_id)
+				elif bool(mobile_no and mobile_no.strip()):
+					receiver_list.append(mobile_no)
+				elif bool(email and email.strip()):
+					receiver_list.append(email)
+
+		return receiver_list
+
 	def send_threema_msg(self, doc, context):
 		send_message(
-			receiver_list=self.get_receiver_list(doc, context),
+			receiver_list=self.get_threema_receiver_list(doc, context),
 			msg=frappe.render_template(self.message, context),
 		)
 
